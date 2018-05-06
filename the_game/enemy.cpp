2bd11dd734e8c837b0ffb3f52f218dd446026ccd
chrispycode <christopher.pauli@mind-me.de>
2018-05-06 20:12:47 +0200

    added new game

---
+++ the_game/enemy.cpp
@@ -0,0 +1,63 @@
+#include "enemy.h"
+#include <QDebug>
+#include <QObject>
+#include <math.h>
+
+Enemy::Enemy(QObject *parent)
+    : QObject(parent)
+
+{
+    xdir = 0;
+    ydir = 1;
+    destroyed = false;
+	image.load(":images/en1.png");
+    rect = image.rect();
+    timerd = startTimer(100);
+    xt = QTime::currentTime().addSecs(1);
+    irot = 1;
+}
+Enemy::~Enemy()
+{
+    killTimer(timerd);
+    qDebug() << "Enemy deleted:" <<  "OK";
+}
+
+void Enemy::resetState(int x,int y)
+{
+	rect.moveTo(x,y);
+}
+void Enemy::autoMove()
+{
+	rect.translate(xdir, ydir);
+
+
+}
+void Enemy::Destroyed()
+{
+    image.load(":images/explo.png");
+    destroyed=true;
+}
+void Enemy::timerEvent(QTimerEvent *event)
+{
+    n = QTime::currentTime();
+    if(xt > n){
+	   image.load(":images/en1.png");
+
+    }
+    if(xt < n){
+		image.load(":images/en2.png");
+		xt = QTime::currentTime().addSecs(1);
+    }
+}
+
+QRect Enemy::getRect(){
+  return rect;
+}
+
+QImage & Enemy::getImage(){
+  return image;
+}
+void Enemy::moveTop(int top)
+{
+  rect.moveTop(top);
+}
---
+++ the_game/the_game.pro
@@ -0,0 +1,26 @@
+QT += widgets multimedia
+
+TEMPLATE = app
+
+RESOURCES += the_game.qrc
+
+CONFIG += debug
+
+TARGET = sescape
+
+OBJECTS_DIR = build
+
+MOC_DIR = $$OBJECTS_DIR
+
+SOURCES += main.cpp \
+    widget.cpp \
+    shoot.cpp \
+    plane.cpp \
+    enemy.cpp
+
+HEADERS += widget.h \
+    shoot.h \
+    plane.h \
+    enemy.h
+
+FORMS += widget.ui
